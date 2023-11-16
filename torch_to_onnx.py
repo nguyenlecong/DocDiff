b74f4ed749902cfa83188af4637cb61cfc753ee7
nguyenlecong <nguyen.lc41@gmail.com>
2023-11-16 11:59:52 +0700

    Set name for onnx model input

--- torch_to_onnx.py
+++ torch_to_onnx.py
@@ -33,7 +33,7 @@ class Converter():
                   export_params=True,        # store the trained parameter weights inside the model file
                   opset_version=10,          # the ONNX version to export the model to
                   do_constant_folding=True,  # whether to execute constant folding for optimization
-                  input_names = ['input'],   # the model's input names
+                  input_names = ['input', 'timestep'],   # the model's input names
                   output_names = ['output'], # the model's output names
                   dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
                                 'output' : {0 : 'batch_size'}})
